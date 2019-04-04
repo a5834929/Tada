547d6ebc2583f8728a15a21e7ff6f31422b5f364
a5834929 <a5834929@gmail.com>
2019-04-04 10:50:08 -0700

    add react-native-element

--- App.js
+++ App.js
@@ -6,24 +6,33 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+ import React, {Component} from 'react';
+ import {Platform, StyleSheet, Text, View} from 'react-native';
+ import { Button } from 'react-native-elements';
+ import Icon from 'react-native-vector-icons/FontAwesome';
 
-const instructions = Platform.select({
+ const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  'Double tap R on your keyboard to reload,\n' +
+  'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+ type Props = {};
+ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+
+        <Button
+          title="Click Me"
+          type="outline"
+        />
+
       </View>
     );
   }
