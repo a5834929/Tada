ee03d37fbb298563466d78e99b62aba85f1c0d3d
Jonathan Ming <jming@pkcsecurity.com>
2019-04-04 12:27:06 -0700

    Include Authorize.NET In-Person SDK & RN bridge library

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 // @flow
 
 import React, { useState } from "react";
-import { Text, View, Button } from "react-native";
+import { NativeModules, Text, View, Button } from "react-native";
 
 export default function App() {
   const [count, setCount] = useState(0);
+
+  const RNAuthNetSdk = NativeModules.RNAuthNetSdk;
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Hi there! You've hit the button {count} times!</Text>
       <Button onPress={() => setCount(count + 1)} title="Press!" />
+      <Button
+        onPress={() => RNAuthNetSdk.doTheThing("cool", "gusto")}
+        title="Mystery Button"
+      />
     </View>
   );
 }
