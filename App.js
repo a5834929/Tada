84c22504b87892755986bfbea89b3444f6c05fba
Jonathan Ming <jming@pkcsecurity.com>
2019-04-11 15:21:59 -0700

    Charge it!

--- App.js
+++ App.js
@@ -1,27 +1,52 @@
 // @flow
 
 import React, { useState } from "react";
-import { NativeModules, Text, View, Button } from "react-native";
+import {
+  NativeModules,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Switch
+} from "react-native";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
   const [resp, setResp] = useState("");
+  const [isLive, setIsLive] = useState(false);
 
   const RNAuthNetSdk = NativeModules.RNAuthNetSdk;
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ margin: 5 }}>
-        Hi there! You've pressed the button {count} times!
-      </Text>
-      <View style={{ margin: 5 }}>
-        <Button onPress={() => setCount(count + 1)} title="Press!" />
-      </View>
+      <TextInput
+        style={{ height: 40, margin: 5 }}
+        onChangeText={setUser}
+        value={user}
+        placeholder="Username"
+        autoCapitalize="none"
+        autoComplete="username"
+        autoFocus={true}
+        textContentType="username"
+      />
+      <TextInput
+        style={{ height: 40, margin: 5 }}
+        onChangeText={setPass}
+        value={pass}
+        placeholder="Password"
+        autoCapitalize="none"
+        autoComplete="password"
+        secureTextEntry={true}
+        textContentType="password"
+      />
+      <Switch onValueChange={setIsLive} style={{ margin: 5 }} value={isLive} />
       <View style={{ margin: 5 }}>
         <Button
           onPress={() =>
-            RNAuthNetSdk.initAuthNet("test").then(itWorked =>
-              setCount(count + itWorked ? 5 : -5)
+            RNAuthNetSdk.initAuthNet(isLive ? "live" : "test", user, pass).then(
+              itWorked =>
+                setResp(`Init ${itWorked ? "worked" : "didn't work"}!`)
             )
           }
           title="Mystery Button"
@@ -30,9 +55,7 @@ export default function App() {
       <Text style={{ margin: 5 }}>{resp.length > 0 ? resp : "Nothing"}</Text>
       <View style={{ margin: 5 }}>
         <Button
-          onPress={() =>
-            RNAuthNetSdk.getUnsettledTxnList().then(res => setResp(res))
-          }
+          onPress={() => RNAuthNetSdk.chargeIt().then(res => setResp(res))}
           title="Another Mystery!"
         />
       </View>
