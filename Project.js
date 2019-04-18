9e270f7e4bf8a284b8cc4a61c2d817d2a98e4dbc
a5834929 <a5834929@gmail.com>
2019-04-18 16:59:12 -0700

    testing give button

--- Project.js
+++ Project.js
@@ -1,9 +1,47 @@
-import React, { Component, useState } from "react";
-import { StyleSheet, View, Text, Image, ActivityIndicator } from "react-native";
+import React, { Component, useState, useEffect } from "react";
+import { StyleSheet, View, Text, Image, ActivityIndicator, NativeModules } from "react-native";
 import { Card, ListItem, Button, Icon } from "react-native-elements";
+import AsyncStorage from "@react-native-community/async-storage";
+import UUIDGenerator from "react-native-uuid-generator";
+
+async function getOrSetDeviceID(setter) {
+  try {
+    console.log("Trying to get device ID");
+    const id = await AsyncStorage.getItem("@deviceID");
+    if (id !== null) {
+      console.log(`Got device ID! ${id}`);
+      return id;
+    } else {
+      console.log("Device ID was null, generating a new one");
+      const newID = await UUIDGenerator.getRandomUUID();
+      console.log(`Generated new UUID: ${newID}`);
+      try {
+        console.log("Trying to set in storage...");
+        await AsyncStorage.setItem("@deviceID", newID);
+        console.log("Set!");
+        return newID;
+      } catch (setE) {
+        console.error(`Error setting device ID in storage! ${setE}`);
+      }
+    }
+  } catch (getE) {
+    console.error(`Error retrieving or generating device ID! ${getE}`);
+  }
+}
 
 export default function ProjectPage(props) {
+  const [text, setText] = useState("GIVE")
+  const [isLive, setIsLive] = useState(false);
+  const [anetIsInit, setAnetIsInit] = useState(false);
+  const [deviceID, setDeviceID] = useState(null);
   const project = props.navigation.getParam('project', {title: "No Project", abstract: "", url: ""});
+
+  useEffect(() => {
+    getOrSetDeviceID().then(setDeviceID);
+    return;
+  }, []);
+
+  const RNAuthNetSDK = NativeModules.RNAuthNetSDK;
 
   return (
     <View style={styles.container}>
@@ -14,7 +52,10 @@ export default function ProjectPage(props) {
           resizeMode={"contain"}
         />
         <Text style={{ marginBottom: 20 }}>{project.abstract}</Text>
-        <Button title="GIVE"/>
+        <Button
+          onPress={() => RNAuthNetSDK.chargeIt().then(res => console.log(res))}
+          title={text}
+        />
       </Card>
     </View>
   );
--- android/app/src/main/java/com/tada/MainApplication.java
+++ android/app/src/main/java/com/tada/MainApplication.java
@@ -3,6 +3,8 @@ package com.tada;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.github.traviskn.rnuuidgenerator.RNUUIDGeneratorPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNGestureHandlerPackage(),
             new RNUUIDGeneratorPackage(),
             new AsyncStoragePackage(),
             new RNAuthNetSDKPackage()
